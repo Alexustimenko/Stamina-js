e4c46ad75c83007978e6768d69581eff1106f51c
Alexustimenko <ustimenko.lesha@gmail.com>
2025-07-01 19:05:44 +0300

    Keyboard press statistics added

--- src/components/Trainer.jsx
+++ src/components/Trainer.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import { terms } from '../data/terms';
 import { TermDisplay } from './TermDisplay';
 import { InputField } from './InputField';
@@ -7,11 +7,33 @@ import { Keyboard } from './Keyboard';
 export const Trainer = () => {
     const [currentIndex, setCurrentIndex] = useState(0);
     const [inputValue, setInputValue] = useState('');
+    const [totalPresses, setTotalPresses] = useState(0);
+    const [correctHits, setCorrectHits] = useState(0);
 
     const currentTerm = terms[currentIndex];
 
+    useEffect(() => {
+        const handleKeyDown = (event) => {
+            if (event.key === 'Escape') {
+                alert(`Stat: All Pressed: ${totalPresses} correct: ${correctHits} Percentage: ${Math.round((correctHits/totalPresses*100),2)} %`);
+            }
+        };
+        window.addEventListener('keydown', handleKeyDown);
+        return () => window.removeEventListener('keydown', handleKeyDown);
+    },[totalPresses,correctHits]);
+
     const handleInputChange = (value) => {
+        const prevLength = inputValue.length;
+        const newChar = value[prevLength];
+
         setInputValue(value);
+        setTotalPresses((prev) => prev + 1);
+
+        const expectedChar = currentTerm[prevLength];
+        if (newChar === expectedChar) {
+            setCorrectHits((prev) => prev + 1);
+        }
+
         if (value === currentTerm) {
             setTimeout(() => {
                 setCurrentIndex((prev) => (prev + 1) % terms.length);
@@ -26,6 +48,23 @@ export const Trainer = () => {
 
         } else {
             setInputValue((prev) => prev + key);
+            setTotalPresses((prev) => prev + 1);
+            const expectedChar = currentTerm[inputValue.length];
+
+            if (key === expectedChar) {
+                setCorrectHits((prev) => prev + 1);
+            }
+
+            const newValue = inputValue + key;
+            setInputValue(newValue);
+
+            if (newValue === currentTerm) {
+                setTimeout(() => {
+                    setCurrentIndex((prev) => (prev + 1) % terms.length);
+                    setInputValue('');
+                }, 300);
+            }
+
         }
     };
 
